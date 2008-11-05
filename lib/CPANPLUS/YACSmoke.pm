9ebaefbfb9c6c17d0cc08ffebebf5127a77846b2
Chris Williams <chris@bingosnet.co.uk>
2008-11-05 09:47:11 +0000

    Import of BINGOS/CPANPLUS-YACSmoke-0.24 from CPAN.
    gitpan-cpan-distribution: CPANPLUS-YACSmoke
    gitpan-cpan-version:      0.24
    gitpan-cpan-path:         BINGOS/CPANPLUS-YACSmoke-0.24.tar.gz
    gitpan-cpan-author:       BINGOS
    gitpan-cpan-maturity:     released

--- lib/CPANPLUS/YACSmoke.pm
+++ lib/CPANPLUS/YACSmoke.pm
@@ -34,7 +34,7 @@ our %EXPORT_TAGS = (
 our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
 our @EXPORT    = ( @{ $EXPORT_TAGS{'default'} } );
 
-$VERSION = '0.22';
+$VERSION = '0.24';
 
 {
   my %Checked;
@@ -70,7 +70,7 @@ sub new {
   $conf->set_conf( prereqs => 2 ); # force to ask callback
   $conf->set_conf( skiptest => 0 ); 
   $conf->set_conf( dist_type => 'CPANPLUS::Dist::YACSmoke' ); # this is where the magic happens.
-  $conf->set_conf( cpantest => 1 ); # Yes, we want to report test results.
+  $conf->set_conf( cpantest => 'dont_cc' ); # Yes, we want to report test results. But not CC
   $conf->set_conf( verbose => 1 ); # set verbosity to true.
 
   unless ( defined $ENV{MAILDOMAIN} ) {
